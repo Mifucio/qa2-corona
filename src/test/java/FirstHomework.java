332e3626ee131d7cd02f953a197c19fde2659ce3
Raimonds Geislers <raimonds.geislers@gmail.com>
2020-12-21 18:11:28 +0200

    Prove1

--- src/test/java/FirstHomework.java
+++ src/test/java/FirstHomework.java
@@ -1,25 +1,12 @@
-import org.junit.jupiter.api.Test;
 
 
 
-public class FirstHomework {
-
-    @Test
-    public void firstHomeWork() {
-    //S=K(1+i)^n
-        //S = sum to return
-        //K = Credited sum
-        //n = number of payment`s periods
-        //i = interest rate (expressed in decimals)
-    float K =  3000;
-    float n = 3;
-    float i = (12/100);
 
 
-    }
-
-
-
-}
+////S=K(1+i)^n
+//S = sum to return
+//K = Credited sum
+//n = number of payment`s periods
+//i = interest rate (expressed in decimals)
 //simbolu skaits jebkurā rindā, vārdu skaits
 //aprēķināt attālumu starp koordinātem kartē
