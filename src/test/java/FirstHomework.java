2b710729e8227bbefc4eee4a79ba89dc8262db47
Raimonds Geislers <raimonds.geislers@gmail.com>
2020-12-23 20:52:57 +0200

    Prove2

--- src/test/java/FirstHomework.java
+++ src/test/java/FirstHomework.java
@@ -1,12 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+public class FirstHomework {
 
 
+    @Test
 
+    public void firstHomework(){
 
+    }
 
-////S=K(1+i)^n
+//S=K(1+i)^n
 //S = sum to return
 //K = Credited sum
 //n = number of payment`s periods
 //i = interest rate (expressed in decimals)
 //simbolu skaits jebkurā rindā, vārdu skaits
 //aprēķināt attālumu starp koordinātem kartē
+
+    //tvnet pāriet uz galveno lapu, pāriet uz rakstu, pāriet uz raksta komentāriem.
+    //*pāriet uz n rakstu pēc kārtas, (izmantojot sarakstus)
+    //**pāriet uz rakstu pēc konkrēta nosaukuma
+    //***pārejot starp lapām pārbaudīt, ka rakstu nosaukumi galvenajā lapā atbilst nosaukumiem konkrētos rakstos, gan komentāros
+    //****kopā ar virsrakstierm pārbaudīt arī komentāru skaitu, ka komentu skaits, kas norādīts galvenajā lapā, atbilst skaitam rakstā un komentos. Parbaudei izmantot Intus nevis Stringus
+    //*****pārbaudot komentāru skaitu pārbaudīt, ka to uzrādītais skaits atbilst faktiski esošajam skaitam.
+
+}
--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,12 +1,15 @@
 import org.junit.jupiter.api.Test;
 
 public class MyFirstTest {
+    private String name = "Gospodin";
 
     @Test
     public void firstTest() {
         System.out.println("Hello, corona!");
+
         int sum = sumTwoDigits(15, 45);
-        System.out.println(sum);
+        sumTwoDigits(sum, 15);
+        System.out.println("And the sum is: " + sum + " and bla bla bla");
 
         System.out.println(sumTwoDigits(9, 13));
 
